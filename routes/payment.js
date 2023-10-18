7e7be4da7521ee067315211947aaddd7e8799e64
rajatsahu18 <sahurajat78@gmail.com>
2023-10-19 00:26:45 +0530

    travel-backend

---
+++ routes/payment.js
@@ -0,0 +1,29 @@
+// import express from "express";
+// import { verifyUser } from "../utils/verifyToken.js";
+// import { createPayment } from '../controllers/paymentController.js'
+
+// const router = express.Router();
+// // verifyUser,
+// router.post("/payment", verifyUser, createPayment);
+
+// export default router;
+
+
+const express = require('express');
+const payment_route = express();
+
+const bodyParser = require('body-parser');
+payment_route.use(bodyParser.json());
+payment_route.use(bodyParser.urlencoded({ extended:false }));
+
+const path = require('path');
+
+payment_route.set('view engine','ejs');
+payment_route.set('views',path.join(__dirname, '../views'));
+
+const paymentController = require('../controllers/paymentController');
+
+payment_route.get('/', paymentController.renderProductPage);
+payment_route.post('/createOrder', paymentController.createOrder);
+
+module.exports = payment_route;
